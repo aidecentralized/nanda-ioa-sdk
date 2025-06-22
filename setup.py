8e935caa7b5a280a435e71c88daf87861fd7bd7d
jinubabu <jinu@cosmaya.co.in>
2025-06-22 09:45:55 +0530

    change the script issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nanda_sdk",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(),
     package_data={
         'nanda_sdk': [
