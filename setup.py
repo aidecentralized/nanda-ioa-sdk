6342d70864d756bd0016c47f338a83bf1e7e2789
jinubabu <jinu@cosmaya.co.in>
2025-06-22 09:59:25 +0530

    fix the restart issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nanda_sdk",
-    version="0.1.7",
+    version="0.1.8",
     packages=find_packages(),
     package_data={
         'nanda_sdk': [
