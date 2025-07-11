31d0dcb2ba27a4f9fb80867e56a42600e5168baf
jinubabu <jinu@cosmaya.co.in>
2025-07-11 14:34:47 +0530

    registry with port

--- nanda_sdk/nanda_sdk.py
+++ nanda_sdk/nanda_sdk.py
@@ -18,7 +18,7 @@ class NandaSdk:
     def __init__(self, 
                  domain: str,
                  num_agents: int,
-                 registry_url: str = "https://chat.nanda-registry.com",
+                 registry_url: str = "https://chat.nanda-registry.com:6900",
                  agent_id: Optional[int] = None):
         """
         Initialize NANDA SDK
