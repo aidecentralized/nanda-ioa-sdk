3421162786de912d17475dece7732fb6b2af9a98
jinubabu <jinu@cosmaya.co.in>
2025-07-11 11:02:18 +0530

    version number change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nanda_sdk",
-    version="0.1.4",
+    version="0.1.5.0",
     packages=find_packages(),
     package_data={
         'nanda_sdk': [
